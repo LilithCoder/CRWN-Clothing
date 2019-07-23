e103d599c98b6a80993caac928e29108f5e35285
Weilin Gao <william.wlgao@gmail.com>
2019-07-23 08:24:30 +0800

    add firebase into our app and add function of signing in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBDV1sYbJvLYjuzp6O6HY9W_ea6UkOl2K4",
+    authDomain: "crwn-db-ae3bb.firebaseapp.com",
+    databaseURL: "https://crwn-db-ae3bb.firebaseio.com",
+    projectId: "crwn-db-ae3bb",
+    storageBucket: "",
+    messagingSenderId: "1008621513715",
+    appId: "1:1008621513715:web:28a8e2bf8c8894b1"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
